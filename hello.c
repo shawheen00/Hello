9fbac1230f4d34b7d4a6290d1644d79bf8c26c99
aalzaki <46790255+aalzaki@users.noreply.github.com>
2019-01-17 14:59:17 -0600

    Update hello.c
    added notes

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
  * Date: 2019/17/01
  *
  * A simple hello world program in C
- *
+ * Editor: Aymen Alzaki
  */
 #include<stdlib.h>
 #include<stdio.h>
@@ -12,8 +12,8 @@ int main(int argc, char **argv) {
 
   printf("Name: Shawheen Naderi\n");
   printf("Major: Finance\n");
-  printf("Name: Aymen Alzaki\n");
-  printf("Major: Computer Science\n");
+  printf("Name: Aymen Alzaki\n");  //what i added 
+  printf("Major: Computer Science\n"); //what i added
 
   return 0;
 }
