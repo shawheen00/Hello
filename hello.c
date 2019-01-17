caf25076d5659a3a518dd5294bdc9de2e24e2dcb
shawheen <shawheen.naderi@huskers.unl.edu>
2019-01-17 14:19:33 -0600

    Update hello.c
    A simple program that prints my name

--- hello.c
+++ hello.c
@@ -1,6 +1,16 @@
+/**
+ * Author: Shawheen Naderi
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
 
-int main (int argc, char **argv){
-	printf("Shawheen Naderi\n");
-	printf("Finance\n");
-	return 0;
+int main(int argc, char **argv) {
+
+  printf("Shawheen Naderi\n");
+
+  return 0;
 }
